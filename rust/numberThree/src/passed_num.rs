e75eccae4d8035439405de1a6f42588f0fa93e2c
iuri-pdista <correaiuri86@gmail.com>
2020-07-28 13:50:13 -0300

    Add logic to find the largest prime factor :poop:

--- rust/numberThree/src/passed_num.rs
+++ rust/numberThree/src/passed_num.rs
@@ -1,4 +1,5 @@
 use crate::primes::is_prime;
+use crate::primes::prime_listing;
 
 pub enum IsPrime{
     Prime,
@@ -7,4 +8,17 @@ pub enum IsPrime{
 
 pub fn prime_decision( num: usize ) -> IsPrime{
     if is_prime(num) { IsPrime::Prime } else { IsPrime::NotPrime }
+}
+
+pub fn _largest_prime_factor(num: usize, ceiling_value: usize)->usize{
+    let _list: Vec<usize> = prime_listing(ceiling_value);
+    let mut vec_size = _list.len();
+    vec_size -= 1;
+    loop{
+        if (num % _list[vec_size]) == 0 {
+            let _largest_prime_factor = _list[vec_size];
+        } else {
+            vec_size -= 1;
+        };
+    }
 }
