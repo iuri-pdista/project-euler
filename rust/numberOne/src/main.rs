37f281f1cf8fed470a909cd0f3df80b28a3844cc
iuri-pdista <correaiuri86@gmail.com>
2020-07-27 23:08:54 -0300

    Add the primes internal crate

---
+++ rust/numberOne/src/main.rs
@@ -0,0 +1,9 @@
+// use std::io;
+// use std::io::prelude::*;
+// use std::env;
+
+mod prime;
+
+fn main() {
+    prime::prime_listing();
+}
---
+++ rust/numberOne/src/prime.rs
@@ -0,0 +1,22 @@
+pub fn prime_listing(/*ceiling_value: usize*/)/*->Vec<u32>*/{
+    let eurt: bool = is_prime(10);
+    println!("{:?}",eurt);
+    // for count in 1..ceiling_value{
+    // };
+}
+
+fn is_prime(num: usize)->bool{
+    let is_prime: bool;
+    is_prime = prime_decision(num);
+    if is_prime { true } else { false }
+}
+
+fn prime_decision(num:usize)->bool{
+    let mut divider_count = 0;
+    for i in 1..num{
+        if (num % i) == 0{
+            divider_count += 1;
+        };
+    };
+    if divider_count == 2 {true} else  {false}
+}
