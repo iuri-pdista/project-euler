6b56dbc343be2e85a5f6738115b146e7bf42d617
iuri-pdista <correaiuri86@gmail.com>
2020-07-28 12:34:21 -0300

    Include the passed_num is prime in the main

--- rust/numberThree/src/main.rs
+++ rust/numberThree/src/main.rs
@@ -1,10 +1,17 @@
+#![warn(dead_code)]
+
 // use std::io;
 // use std::io::prelude::*;
-// use std::env;
 
 mod primes;
+mod passed_num;
+use crate::passed_num::IsPrime;
 
-fn main() {
-    let list: Vec<usize>= primes::prime_listing(10);
-    println!("{:?}",list);
+fn main() {  
+    let _list: Vec<usize> = primes::prime_listing(100);
+    let decision: IsPrime= passed_num::prime_decision(5);
+    match decision{
+        IsPrime::Prime=> {println!("The biggest prime factor is alo")},
+        IsPrime::NotPrime=> {println!("ola")},
+    }
 }
