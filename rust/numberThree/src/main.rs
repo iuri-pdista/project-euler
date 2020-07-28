b61948477432293df90864cd3f5331bc1ce9b9c4
iuri-pdista <correaiuri86@gmail.com>
2020-07-28 13:08:03 -0300

    Add input reader ps Everything is working :white_check_mark:

--- rust/numberThree/src/main.rs
+++ rust/numberThree/src/main.rs
@@ -1,17 +1,23 @@
 #![warn(dead_code)]
 
-// use std::io;
-// use std::io::prelude::*;
+use std::io;
+use std::io::prelude::*;
 
 mod primes;
 mod passed_num;
 use crate::passed_num::IsPrime;
 
 fn main() {  
+    let mut num_string = String::new();
+    io::stdin()
+        .read_line(&mut num_string)
+        .expect("Failed to read number");
+
+    let mut _num: usize = num_string.trim_end().parse().unwrap();
     let _list: Vec<usize> = primes::prime_listing(100);
-    let decision: IsPrime= passed_num::prime_decision(5);
+    let decision: IsPrime= passed_num::prime_decision(_num);
     match decision{
-        IsPrime::Prime=> {println!("The biggest prime factor is alo")},
+        IsPrime::Prime=> {println!("The biggest prime factor is {}", _num)},
         IsPrime::NotPrime=> {println!("ola")},
     }
 }
