876fa8609e7eb6014a42b75e575f2254128517fb
iuri-pdista <correaiuri86@gmail.com>
2020-08-11 00:09:16 -0300

    Imported prime handling logic from problem number 3

---
+++ rust/numberTen/src/prime_handler.rs
@@ -0,0 +1,24 @@
+pub fn prime_listing(ceiling_value: usize)->Vec<usize>{
+    let mut prime_list = Vec::with_capacity(ceiling_value);
+    prime_list.push(1);
+    for count in 0..=ceiling_value{
+        if is_prime(count) {prime_list.push(count)};
+    };
+    prime_list
+}
+
+pub fn is_prime(num: usize)->bool{
+    let is_prime: bool;
+    is_prime = prime_decision(num);
+    if is_prime { true } else { false }
+}
+
+fn prime_decision(num:usize)->bool{
+    let mut divider_count = 0;
+    for i in 1..num{
+        if (num % i) == 0{
+            divider_count += 1;
+        };
+    };
+    if divider_count == 1 { true } else { false }
+}
